21b9e577326f0e30e179a92652d09123f629a9a5
harounc <coulibalyharouna19@gmail.com>
2024-04-23 17:16:10 +0000

    gestionnaire visiteur V1

---
+++ gestionnaire_visiteur_v1.py
@@ -0,0 +1,115 @@
+# Implémente les multi-visites
+
+from datetime import datetime
+
+MENUACTIF = 0
+LISTE_VISITEURS = []
+
+# Définition du menu 0
+def menuPrincipal():
+  global MENUACTIF
+  print(
+    """
+    Bienvenu sur VISITORIA \n
+    1. Enregistrer une visite \n
+    2. Terminer une visite \n
+    3. Voir les visites en cours \n
+    """
+  )
+  choix = input("selectioneé une option: ")
+  if choix == "1":
+    MENUACTIF = 1
+
+  elif choix =="2":
+    MENUACTIF = 2
+
+  elif choix =="3":
+    MENUACTIF = 3
+
+# Définition du menu 1
+# supposont que le nom et prenoms sont uniques
+
+def menuEnregistrerVisite():
+  global MENUACTIF
+
+  NomPrenom = input("Nom et prénoms: ")
+  trouver = False
+  for visiteur in LISTE_VISITEURS :
+    if NomPrenom == visiteur["Nom et Prenoms"] :
+      trouver = True
+      visite = {}
+      visite["Motif Visite"] = input("Motif Visite: ")
+      visite["Heure entrée"] = datetime.now()
+      id_visite = len(visiteur["Visites"])
+      visiteur["Visites"][id_visite] = visite
+
+
+  if trouver == False :
+      visiteur = {
+                  "Visites":{}
+                  }
+      visiteur["Nom et Prenoms"] = NomPrenom
+      visiteur["Type de pièce"] = input("Type de pièce: ")
+      visiteur["Num pièce"] = input("Num pièce: ")
+
+      visite = {}
+      visite["Motif Visite"] = input("Motif Visite: ")
+      visite["Heure entrée"] = datetime.now()
+      visite["Heure sortie"] = None
+      id_visite = len(visiteur["Visites"])
+      visite["id"] = id_visite
+
+      visiteur["Visites"][id_visite] = visite
+
+      LISTE_VISITEURS.append(visiteur)
+
+  MENUACTIF = 0
+
+# Définition du menu 2
+def menuTerminerVisite():
+  global MENUACTIF
+  ID_visite = int(input("entreé ID de la visite: "))
+  for visiteur in LISTE_VISITEURS :
+    #print("ID_visite", ID_visite, type(ID_visite))
+    #print("clés", list(visiteur["Visites"]))
+
+    if visiteur["Visites"].get(ID_visite) :
+      Terminer = input("taper 'q' pour terminer: ")
+      while Terminer != 'q' :
+        Terminer = input("taper 'q' pour terminer: ")
+      visiteur["Visites"][ID_visite]["Heure sortie"] = datetime.now()
+  MENUACTIF = 0
+
+# Définition du menu 3 : voir les visites en cours
+def menuVisiteEnCours():
+  global MENUACTIF
+
+  for visiteur in LISTE_VISITEURS :
+    for visite in visiteur["Visites"].values():
+      #print(visite)
+      #print(visiteur)
+      if visite["Heure sortie"] == None :
+        print(f'{visiteur["Nom et Prenoms"]} {visite["id"]} {visite["Heure entrée"]}')
+
+  MENUACTIF = 0
+
+
+#corps du programme
+while True:
+  if MENUACTIF == 0:
+    menuPrincipal()
+
+  elif MENUACTIF == 1:
+    menuEnregistrerVisite()
+
+  elif MENUACTIF == 2:
+    menuTerminerVisite()
+
+  elif MENUACTIF == 3:
+    menuVisiteEnCours()
+
+  # Terminer le programme
+  if MENUACTIF == -1:
+    break
+
+print("\n\n\t Fin du programme !")
